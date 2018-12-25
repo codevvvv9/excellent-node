0fda76ea8ca6cdd40acb6304723107c1ad795a17
shaolin-wu <15265249516@163.com>
2018-12-25 23:58:24 +0800

    What: use module.exports
    Why:
    
    *
    
    How:
    
    * s

--- http-demo.js
+++ http-demo.js
@@ -1,6 +1,7 @@
 const http = require('http');
 // 解析url中？之后的键值对
 const url = require('url');
+const parseUrl = require('./parseUrl');
 
 const users = [];
 // 假设里面的用户是如下结构
@@ -9,57 +10,6 @@ const users = [];
 //   age: 19,
 // }
 const server = http.createServer();
-
-/**
- * 解析url
- * @param request
- * @param response
- */
-const parseUrl = (request, response) => {
-  const { requestUrl } = { requestUrl: url.parse(request.url, true) };
-  const { method } = { method: request.method };
-  let userName = '';
-  let user = '';
-  let requestData = '';
-  switch (requestUrl.pathname) {
-    case '/user':
-      switch (method) {
-        case 'GET':
-          userName = requestUrl.query.name ? requestUrl.query.name : '';
-          user = users.find(userItem => userItem.name === userName);
-          response.end(JSON.stringify(user));
-          break;
-        case 'POST':
-          request.on('data', (data) => {
-            requestData += data.toString('utf-8');
-          });
-          request.on('end', () => {
-            let userStr;
-            try {
-              userStr = JSON.parse(requestData);
-            } catch (e) {
-              response.statusCode = 400;
-              response.end(JSON.stringify({ err: 'you sent a bad request' }));
-            }
-            users.push(userStr);
-            response.end(JSON.stringify({
-              status: 'succeed',
-            }));
-          });
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      response.statusCode = 404;
-      response.end(JSON.stringify({
-        status: 'not found page',
-      }));
-      break;
-  }
-};
-
 server.on('request', (request, response) => {
   parseUrl(request, response);
 });
---
+++ parseUrl.js
@@ -0,0 +1,51 @@
+/**
+ * 解析url
+ * @param request
+ * @param response
+ */
+const parseUrl = (request, response) => {
+  const { requestUrl } = { requestUrl: url.parse(request.url, true) };
+  const { method } = { method: request.method };
+  let userName = '';
+  let user = '';
+  let requestData = '';
+  switch (requestUrl.pathname) {
+    case '/user':
+      switch (method) {
+        case 'GET':
+          userName = requestUrl.query.name ? requestUrl.query.name : '';
+          user = users.find(userItem => userItem.name === userName);
+          response.end(JSON.stringify(user));
+          break;
+        case 'POST':
+          request.on('data', (data) => {
+            requestData += data.toString('utf-8');
+          });
+          request.on('end', () => {
+            let userStr;
+            try {
+              userStr = JSON.parse(requestData);
+            } catch (e) {
+              response.statusCode = 400;
+              response.end(JSON.stringify({ err: 'you sent a bad request' }));
+            }
+            users.push(userStr);
+            response.end(JSON.stringify({
+              status: 'succeed',
+            }));
+          });
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      response.statusCode = 404;
+      response.end(JSON.stringify({
+        status: 'not found page',
+      }));
+      break;
+  }
+};
+
+module.exports.parseUrl = parseUrl;
