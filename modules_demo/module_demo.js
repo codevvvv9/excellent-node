c5eeb3322c2b15b8bfed62e16822cb823402ecf7
shaolin-wu <15265249516@163.com>
2019-01-04 23:52:25 +0800

    What: exports & module.exports
    Why:
    
    *
    
    How:
    
    *

--- modules_demo/module_demo.js
+++ modules_demo/module_demo.js
@@ -1,5 +1,20 @@
-console.log('before exports');
+/* eslint-disable prefer-destructuring */
+// console.log('before exports');
 
-module.exports = () => console.log('hi I\'m module demo1');
+// module.exports = () => console.log('hi I\'m module demo1');
 
-console.log('after exports');
+// console.log('after exports');
+/**
+ * the define of exports
+ */
+// exports = module.exports; // exports is actually a module.exports reference.
+// exports.a = 1;
+// console.log(exports); // { a: 1}
+// console.log(module.exports); // { a: 1}
+/**
+ * change modules.exports reference
+ */
+exports.a = 1;
+module.exports = { b: 2 };
+// console.log(exports); // { a: 1}
+// console.log(module.exports); // { a: 1 }
--- modules_demo/module_demo_2.js
+++ modules_demo/module_demo_2.js
@@ -1,13 +1,7 @@
-console.log(require);
-
-console.log('before require');
-
 const moduleDemo1 = require('./module_demo');
 
-console.log('after require');
-
-moduleDemo1();
+console.log(moduleDemo1);
 // require('./module_demo'); // only first require does
-console.log(require);
+// console.log(require);
 
-console.log(require.cache['/home/wsl/Documents/node-playground/modules_demo/module_demo.js']);
+// console.log(require.cache['/home/wsl/Documents/node-playground/modules_demo/module_demo.js']);
